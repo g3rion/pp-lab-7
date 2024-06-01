ac2da2124ee08a4fd9d818393c743b0140bb0f0f
g3rion <gerard.kulawik@gmail.com>
2024-06-01 11:58:14 +0200

    zad4

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,10 @@ import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main extends Application {
     private TextField directoryPathField;
@@ -54,6 +57,7 @@ public class Main extends Application {
 
     private void searchFiles() {
         String directoryPath = directoryPathField.getText();
+        String searchPhrase = searchField.getText();
         if (directoryPath == null || directoryPath.isEmpty()) {
             resultArea.setText("Please provide a directory path.");
             return;
@@ -66,18 +70,32 @@ public class Main extends Application {
         }
 
         StringBuilder results = new StringBuilder();
-        listFilesInDirectory(directory, results);
+        searchInDirectory(directory, searchPhrase, results);
         resultArea.setText(results.toString());
     }
 
-    private void listFilesInDirectory(File directory, StringBuilder results) {
+    private boolean containsPhrase(File file, String searchPhrase) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(searchPhrase)) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private void searchInDirectory(File directory, String searchPhrase, StringBuilder results) {
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
-                if (file.isFile()) {
+                if (file.isFile() && containsPhrase(file, searchPhrase)) {
                     results.append(file.getAbsolutePath()).append("\n");
                 } else if (file.isDirectory()) {
-                    listFilesInDirectory(file, results);
+                    searchInDirectory(file, searchPhrase, results);
                 }
             }
         }
